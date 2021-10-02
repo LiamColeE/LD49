ce5cc5c85a42a7eefc514c2528c265bfbc547d59
Dan <me@danielholcomb.com>
2021-10-01 20:23:53 -0600

    Rock class and spawner added

---
+++ Assets/DansFolder/Rock.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    public bool Locked { get; set; }
+
+    [SerializeField] Mesh[] meshPool;
+
+    MeshFilter mesh;
+    MeshCollider meshCollider;
+
+    void Awake()
+    {
+        meshCollider = GetComponent<MeshCollider>();
+        mesh = GetComponent<MeshFilter>();
+    }
+
+    void Start()
+    {
+        int meshIndex = Random.Range(0, meshPool.Length);
+
+        mesh.mesh = meshPool[meshIndex];
+        meshCollider.sharedMesh = mesh.mesh;
+
+    }
+
+
+
+}
