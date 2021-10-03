9c3d727e1166adbda1adbb18db48d38325fc5401
LiamColeE <liamedlinger@gmail.com>
2021-10-02 19:19:35 -0500

    moved scripts and added settings script

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    public static float CameraMoveSpeed;
+    public static float CameraZoomSpeed;
+    public static float RockMoveSpeed;
+}
